c84f718e72dc84527a97d8bf3d0ca1b21a9ce630
isoutofreach <102778728+isoutofreach@users.noreply.github.com>
2022-04-02 09:24:38 +0800

    Add files via upload

---
+++ class to python/base day 01/08 运算符.py
@@ -0,0 +1,24 @@
+"""# 计算运算符
+x = 10
+print(10 % 3)"""
+
+
+"""a = 10
+a += 1    # 自增长 a = a + 1
+print(a)
+a -= 1    # 自减少 a = a - 1
+print(a)"""
+
+"""# 逻辑运算符 与运算 and  或运算 or  非运算 not
+user = "rain"
+pwd = 123
+print(user == "yuan" and pwd == "123")"""
+
+# 零值：每一个数据类型都有一个布尔为false的值
+# 整形的零值:0   字符串的零值: ""  列表的零值是空列表  字典的零值是空字典
+# 内置函数：bool
+print(bool(1))
+print(bool(0))    # 整形下只有0的布尔值为false，其他的都是true
+print(bool("false"))
+print(bool(""))    # 字符串下只有空字符串为false，其他都是true
+
---
+++ class to python/base day 01/hello.py
@@ -0,0 +1,3 @@
+print("hello yuan")
+print("hello yuan")
+print("hello yuan")
